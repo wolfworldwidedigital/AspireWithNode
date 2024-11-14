8b970675730124a954e7186fcffae0bb997de53e
David De Wolf <admin@wolfworldwide.digital>
2024-11-14 22:26:08 +0100

    first commit

---
+++ AspireWithNode.AppHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var weatherapi = builder.AddProject<Projects.AspireWithNode_AspNetCoreApi>("weatherapi");
+
+var frontend = builder.AddNpmApp("frontend", "../NodeFrontend", "watch")
+    .WithReference(weatherapi)
+    .WaitFor(weatherapi)
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+var launchProfile = builder.Configuration["DOTNET_LAUNCH_PROFILE"] ??
+                    builder.Configuration["AppHost:DefaultLaunchProfileName"]; // work around https://github.com/dotnet/aspire/issues/5093
+
+if (builder.Environment.IsDevelopment() && launchProfile == "https")
+{
+    frontend.RunWithHttpsDevCertificate("HTTPS_CERT_FILE", "HTTPS_CERT_KEY_FILE");
+}
+
+builder.Build().Run();
